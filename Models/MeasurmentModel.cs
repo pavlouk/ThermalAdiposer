c00bd07b434c2414e08c91c6f237aa3dec50322f
ploukareas <ploukareas@gmail.com>
2022-09-29 18:09:52 +0300

    Added area measurments

---
+++ Models/MeasurmentModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThermalAdiposer.Models
+{
+    public class MeasurmentModel: INotifyPropertyChanged
+    {
+		private string _name;
+		private float _value;
+		private float _min;
+		private float _max;
+		private float _average;
+		private int _x;
+		private int _y;
+		private int _width;
+		private int _height;
+
+		public string Name
+		{
+			get { return _name; }
+			set 
+			{ 
+				_name = value;
+				OnPropertyChanged(nameof(Name));
+			}
+		}
+
+		public float Value
+		{
+			get { return _value; }
+			set 
+			{ 
+				_value = value;
+				OnPropertyChanged(nameof(Value));
+			}
+		}
+
+		public float Min
+		{
+			get { return _min; }
+			set 
+			{ 
+				_min = value;
+				OnPropertyChanged(nameof(Min));
+			}
+		}
+
+		public float Max
+		{
+			get { return _max; }
+			set 
+			{ 
+				_max = value;
+				OnPropertyChanged(nameof(Max));
+			}
+		}
+
+		public float Average
+		{
+			get { return _average; }
+			set 
+			{ 
+				_average = value;
+				OnPropertyChanged(nameof(Average));
+			}
+		}
+
+		public int X
+		{
+			get { return _x; }
+			set 
+			{ 
+				_x = value;
+				OnPropertyChanged(nameof(X));
+			}
+		}
+
+		public int Y
+		{
+			get { return _y; }
+			set 
+			{ 
+				_y = value;
+				OnPropertyChanged(nameof(Y));
+			}
+		}
+
+		public int Width
+		{
+			get { return _width; }
+			set 
+			{ 
+				_width = value;
+				OnPropertyChanged(nameof(Width));
+			}
+		}
+
+		public int Height
+		{
+			get { return _height; }
+			set 
+			{ 
+				_height = value;
+				OnPropertyChanged(nameof(Height));
+			}
+		}
+
+		#region INotifyPropertyChanged Members  
+
+		public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ ViewModels/MeasurmentViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ThermalAdiposer.Models;
+
+namespace ThermalAdiposer.ViewModels
+{
+    public class MeasurmentViewModel
+    {
+        private MeasurmentModel _selectedMeasurment;
+
+        public MeasurmentModel SelectedMeasurment
+        {
+            get { return _selectedMeasurment; }
+            set { _selectedMeasurment = value; }
+        }
+        private ObservableCollection<MeasurmentModel> _measurments = new ObservableCollection<MeasurmentModel>();
+
+        public ObservableCollection<MeasurmentModel> Measurments
+        {
+            get { return _measurments; }
+            set { _measurments = value; }
+        }
+
+        public MeasurmentViewModel()
+        {
+            _measurments.Add(new MeasurmentModel { Name = "Bx1", Min = 30.0f, Max = 32.3f, Average = 31.2f, X = 10, Y = 56, Width = 24, Height = 50 });
+            _measurments.Add(new MeasurmentModel { Name = "Bx2", Min = 20.0f, Max = 22.3f, Average = 21.2f, X = 20, Y = 76, Width = 34, Height = 60 });
+            _measurments.Add(new MeasurmentModel { Name = "Li1", Min = 23.0f, Max = 28.3f, Average = 25.2f, X = 30, Y = 66, Width = 44, Height = 70 });
+            _measurments.Add(new MeasurmentModel { Name = "Sp1", Value = 34.0f, X = 30, Y = 66 });
+            _measurments.Add(new MeasurmentModel { Name = "Sp2", Value = 34.0f, X = 30, Y = 66 });
+        }
+        private ICommand _updateCommand;
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (_updateCommand == null)
+                    return new Updater();
+                return _updateCommand;
+            }
+            set
+            {
+                _updateCommand = value;
+            }
+        }
+
+        private class Updater : ICommand
+        {
+            #region ICommand Members  
+
+            public bool CanExecute(object parameter) => true;
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+
+            }
+
+            #endregion
+        }
+    }
+}
